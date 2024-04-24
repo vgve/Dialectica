4aae417e00d92165de78573166eba2c941237f15
Victoria Grinberg <31312528+vicgcode@users.noreply.github.com>
2024-04-25 00:14:59 +0300

    Update navigation

--- app/src/main/java/com/example/dialectica/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dialectica/presentation/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.dialectica.presentation
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.dialectica.R
 import com.example.dialectica.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.dialectica.utils.TAG
+import com.example.dialectica.utils.viewModelFactory
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    private val viewModel: MainViewModel by viewModels(
+        factoryProducer = {
+            viewModelFactory {
+                MainViewModel(
+                    MyApplication.appModule.sharedPrefsRepository
+                )
+            }
+        }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +36,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        setupNavigation()
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        observeUiAction()
+    }
 
-        navView.setupWithNavController(navController)
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+
+    private fun observeUiAction() {
+        lifecycleScope.launch {
+            this@MainActivity.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiAction.collect { uiAction ->
+                    when (uiAction) {
+                        is MainAction.OpenAuthScreen -> {
+                            Log.e(TAG, "OpenAuthScreen")
+                            val navGraph = navController.navInflater.inflate(R.navigation.start_graph)
+                            navGraph.setStartDestination(R.id.signUpFragment)
+                            navController.graph = navGraph
+                        }
+                        is MainAction.OpenHomeScreen -> {
+                            val navGraph = navController.navInflater.inflate(R.navigation.start_graph)
+                            navGraph.setStartDestination(R.id.baseFlowFragment)
+                            navController.graph = navGraph
+                        }
+                    }
+                }
+            }
+        }
     }
 }
