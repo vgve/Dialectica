b5531caf175832df48d09ca5c3906b627bbf8d84
Victoria Grinberg <31312528+vgve@users.noreply.github.com>
2025-07-24 16:10:45 +0300

    Create compose app entry point

---
+++ app/src/main/java/com/vicgcode/dialectica/presentation/App.kt
@@ -0,0 +1,16 @@
+package com.vicgcode.dialectica.presentation
+
+import android.app.Application
+import com.example.dialectica.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
--- app/src/main/java/com/vicgcode/dialectica/presentation/MainActivity.kt
+++ app/src/main/java/com/vicgcode/dialectica/presentation/MainActivity.kt
@@ -1,72 +1,43 @@
 package com.vicgcode.dialectica.presentation
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import com.vicgcode.dialectica.R
-import com.vicgcode.dialectica.databinding.ActivityMainBinding
-import com.vicgcode.dialectica.presentation.extensions.TAG
-import com.vicgcode.dialectica.utils.viewModelFactory
-import kotlinx.coroutines.launch
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vicgcode.dialectica.presentation.navigation.NavGraph
+import com.vicgcode.dialectica.presentation.ui.theme.DialecticaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
-
-    private val viewModel: MainViewModel by viewModels(
-        factoryProducer = {
-            viewModelFactory {
-                MainViewModel(
-                    MyApplication.appModule.sharedPrefsRepository
-                )
-            }
-        }
-    )
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setupNavigation()
-
-        observeUiAction()
-    }
-
-    private fun setupNavigation() {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-    }
-
-
-    private fun observeUiAction() {
-        lifecycleScope.launch {
-            this@MainActivity.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.uiAction.collect { uiAction ->
-                    when (uiAction) {
-                        is MainAction.OpenAuthScreen -> {
-                            Log.e(TAG, "OpenAuthScreen")
-                            val navGraph = navController.navInflater.inflate(R.navigation.start_graph)
-                            navGraph.setStartDestination(R.id.signUpFragment)
-                            navController.graph = navGraph
-                        }
-                        is MainAction.OpenHomeScreen -> {
-                            val navGraph = navController.navInflater.inflate(R.navigation.start_graph)
-                            navGraph.setStartDestination(R.id.baseFlowFragment)
-                            navController.graph = navGraph
-                        }
-                    }
-                }
+        enableEdgeToEdge()
+        setContent {
+            DialecticaTheme() {
+                NavGraph()
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DialecticaTheme() {
+        Greeting("Android")
     }
 }
